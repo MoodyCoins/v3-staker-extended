2445b2cc06d27dcfd695419f71b571f6cf46d276
moody <moodycoinz@protonmail.com>
2022-07-19 03:14:57 -0700

    link

--- src/flat/V3StakerExtendedFlat.sol
+++ src/flat/V3StakerExtendedFlat.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity =0.7.6;
+// see: https://github.com/MoodyCoins/v3-staker-extended
 
+pragma solidity =0.7.6;
 pragma abicoder v2;
 
 /**
